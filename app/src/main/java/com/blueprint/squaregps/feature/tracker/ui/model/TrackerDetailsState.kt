66fcf770aba1e92e7185f69f3a5b154566588dc9
Evgeniy-Zh <othz@mail.ru>
2026-05-14 16:26:16 +0200

    fix import

--- app/src/main/java/com/blueprint/squaregps/feature/tracker/ui/model/TrackerDetailsState.kt
+++ app/src/main/java/com/blueprint/squaregps/feature/tracker/ui/model/TrackerDetailsState.kt
@@ -2,7 +2,6 @@ package com.blueprint.squaregps.feature.tracker.ui.model
 
 import com.blueprint.squaregps.feature.tracker.domain.model.Tracker
 import com.blueprint.squaregps.feature.tracker.domain.model.TrackerState
-import com.blueprint.squaregps.feature.tracker.domain.model.TrackerWithState
 
 data class TrackerDetailsState (
     val tracker: Tracker? = null,
